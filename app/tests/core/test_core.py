6e45cd6ebb465befb9447d0a0d2588b5dc50dd0d
dataGeek0104 <aman.omi010496@gmail.com>
2025-06-09 21:49:11 +0000

    added test for db connection - v2

--- app/tests/core/test_core.py
+++ app/tests/core/test_core.py
@@ -1,28 +1,35 @@
+import os
+
 import pytest  # type: ignore[import-not-found]
 from flask import Flask  # type: ignore[import-not-found]
 from src.app.core.routes import api_bp
+from src.app.extensions import db
 
 
 @pytest.fixture
 def client():
     app = Flask(__name__)
-    app.register_blueprint(api_bp)
     app.config["TESTING"] = True
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.init_app(app)
+    app.register_blueprint(api_bp)
+
+    with app.app_context():
+        db.create_all()
+
     with app.test_client() as client:
         yield client
 
 
 def test_health_check(client):
     response = client.get("/health-check")
-    assert (
-        response.status_code == 200
-    )  # Optional: use `assert response.status_code == 200  # nosec`
+    assert response.status_code == 200  # nosec
     assert response.json == {"message": "Yay! The app is working fine!"}  # nosec
 
 
 def test_db_conn_check(client):
     response = client.get("/db-conn-check")
-    assert (
-        response.status_code == 200
-    )  # Optional: use `assert response.status_code == 200  # nosec`
+    assert response.status_code == 200  # nosec
     assert response.json == {"message": "Yay! The DB is working fine!"}  # nosec
